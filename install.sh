3ac1643780f9de05dc759008dd8f3841ac155dda
Random228 <ranbom89@gmail.com>
2023-07-21 14:26:58 +0300

    commit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ `whoami` = "root" ]]; then 
+if [[ whoami == root ]]; then 
 mkdir /IaC
 wget --progress=bar -c --directory-prefix=/IaC https://github.com/Random228/MyIaC/blob/main/iac.sh 
 wget --progress=bar -c --directory-prefix=/IaC https://github.com/Random228/MyIaC/blob/main/applist
