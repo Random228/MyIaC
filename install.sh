c59ca46bb1cc72aca4a42fe6f1947277dcbb4516
Random228 <ranbom89@gmail.com>
2023-07-21 14:39:29 +0300

    commit

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ if [[ `whoami` == "root" ]]; then
 mkdir /IaC
 wget --progress=bar -c --directory-prefix=/IaC https://github.com/Random228/MyIaC/blob/main/iac.sh 
 wget --progress=bar -c --directory-prefix=/IaC https://github.com/Random228/MyIaC/blob/main/applist
-#bash /IaC/iac.sh
+bash /IaC/iac.sh
 else echo "Скрипт необходимо запускать под пользователем root"
 fi
